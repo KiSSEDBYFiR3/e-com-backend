7873e5788abbea75c4f3e40c123954920cc2a3d0
Stanislav Moiseev <stas90forewer@mail.ru>
2023-12-28 21:02:22 +0300

    fixed order create

--- lib/data/repository/order_repository.dart
+++ lib/data/repository/order_repository.dart
@@ -1,16 +1,34 @@
 import 'package:conduit_core/conduit_core.dart';
 
 import 'package:ecom_backend/data/model/cart.dart';
+import 'package:ecom_backend/data/model/cart_product.dart';
 import 'package:ecom_backend/domain/repository/order_repository.dart';
+import 'package:ecom_backend/util/app_error_response.dart';
 
 class OrderRepository implements IOrderRepository {
   @override
   Future<String> createOrder(
       {required ManagedContext context, required String userId}) async {
     try {
-      final query = Query<Cart>(context)..where((x) => x.user?.userId = userId);
-      await query.delete();
-      return '';
+      await context.transaction(
+        (transaction) async {
+          final query = Query<Cart>(transaction)
+            ..where((x) => x.user?.userId).equalTo(userId);
+
+          final cart = await query.fetchOne();
+
+          if (cart == null) {
+            throw AppResponse.badRequest();
+          }
+
+          final cartProductsQuery = Query<CartProduct>(transaction)
+            ..where((x) => x.cart?.id).equalTo(cart.id);
+
+          final count = await cartProductsQuery.delete();
+          print(count);
+        },
+      );
+      return 'success';
     } on QueryException catch (_) {
       rethrow;
     }
